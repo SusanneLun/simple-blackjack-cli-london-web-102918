6096361ec2903cb3afb2be6f0ee8b225e5158922
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-24 15:38:06 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,13 +50,14 @@ end
 #####################################################
 
 def runner
- welcome 
- initial_round
- hit?
- display_card_total
- if display_card_total > 21
- end_game
+ welcome
+  card_total = initial_round
+  until card_total > 21
+  card_total = hit?(card_total)
+  display_card_total(card_total)
+end	
 end
-end
+    	end_game(card_total)
+end 
 
     
